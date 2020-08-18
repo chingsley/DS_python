9b2833202dfe6c31250d79f01a3006969c1963ec
chingsley <eneja.kc@gmail.com>
2020-08-18 08:31:19 +0100

    completed SQL basics

--- scripts.sql
+++ scripts.sql
@@ -184,9 +184,34 @@
 
 -- NOTICE: using BETWEEN is tricky for dates! While BETWEEN is generally inclusive of endpoints, it assumes the time is at 00:00:00 (i.e. midnight) for dates. This is the reason why we set the right-side endpoint of the period at '2017-01-01' instead of '2016-12-31'
 --^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+--SELECT *
+--FROM web_events
+--WHERE channel IN ('organic', 'adword') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01';
+
+
+
+--  Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the resulting table.
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+--SELECT id
+--FROM orders
+--WHERE gloss_qty > 4000 OR poster_qty > 4000;
+
+
+--  Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000..
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+--SELECT *
+--FROM orders
+--WHERE standard_qty = 0 AND (gloss_qty > 1000 OR poster_qty > 1000);
+
+
+--   Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
 SELECT *
-FROM web_events
-WHERE channel IN ('organic', 'adword') AND occurred_at BETWEEN '2016-01-01' AND '2017-01-01';
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%')
+			AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+			AND primary_poc NOT LIKE '%eana%');
+
 
 
 
