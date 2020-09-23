cae9960234a3ce31b3817cf37ea969ccf7084247
chingsley <eneja.kc@gmail.com>
2020-09-23 21:11:53 +0100

    paractice aggreations: MIN, MAX, AVG

--- scripts.sql
+++ scripts.sql
@@ -692,7 +692,7 @@
 
 
 -- to classify orders into general groups based on order size and count up all the orders in each group for inventory planning
---^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
 --SELECT 	CASE WHEN total > 500 THEN 'Over 500'
 --			 WHEN total > 300 AND total <= 500 THEN '301 - 500'
 --			 WHEN total > 100 AND total <= 300 THEN '101 - 300'
@@ -700,6 +700,70 @@
 --		count(*) AS order_count_in_grp	
 --FROM orders
 --GROUP BY total_group;
+
+
+
+-- When was the earliest order ever placed? You only need to return the date:
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+
+---- -- METHOD 1 - using aggregation function (MIN)
+----^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+--SELECT MIN(occurred_at) AS occurred_at
+--FROM orders;
+
+---- -- METHOD 2 - without using aggregation function
+----^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+--SELECT 	occurred_at
+--FROM orders
+--ORDER BY occurred_at
+--LIMIT 1;
+
+
+-- When did the most recent (latest) web_event occur?:
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+
+---- -- METHOD 1 - using aggregation function (MAX)
+----^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+--SELECT MAX(occurred_at) AS occurred_at
+--FROM web_events;
+--
+---- -- METHOD 2 - without using aggregation function
+----^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+--SELECT 	occurred_at
+--FROM web_events
+--ORDER BY occurred_at DESC
+--LIMIT 1;
+
+
+
+--  Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+--SELECT AVG(standard_qty) mean_standard, AVG(gloss_qty) mean_gloss, AVG(poster_qty) mean_poster,
+--		AVG(standard_amt_usd) mean_standard_amt, AVG(gloss_amt_usd) mean_gloss_amt,
+--		AVG(poster_amt_usd) mean_poster_amt 
+--FROM orders;
+
+
+
+--  what is the MEDIAN total_usd spent on all orders?
+--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--
+--SELECT *
+--FROM   (SELECT total_amt_usd 
+--		FROM orders
+--		ORDER BY total_amt_usd
+--		LIMIT 3457) AS Table1
+--ORDER BY total_amt_usd DESC
+--LIMIT 2;
+--
+---- EXPLANATION FOR THE MEDIAN SOLUTION ABOVE
+----Since there are 6912 orders - we want the average of the 3457 and 3456 order amounts when
+----ordered. This is the average of 2483.16 and 2482.55. This gives the median of 2482.855. This
+----obviously isn't an ideal way to compute. If we obtain new orders, we would have to change the
+----limit. SQL didn't even calculate the median for us. The above used a SUBQUERY, but you could
+----use any method to nd the two necessary values, and then you just need the average of them.
+
+
+
 
 
 
